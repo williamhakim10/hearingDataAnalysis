cd8d6ffa3db9c2834805ccb8d1d4402b8bbadb99
William <williamhakim10@gmail.com>
2017-08-22 18:18:11 -0400

    initial push

---
+++ filter.py
@@ -0,0 +1,25 @@
+import csv
+import sys
+import argparse as ap
+
+parser = ap.ArgumentParser('Filter a CSV')
+parser.add_argument('inFile', help='File to be filtered')
+parser.add_argument('-f', '--filter', metavar='PHRASE', nargs='+', default=[], help='List of words and phrases to search for, and remove resulting sentences from output CSV')
+parser.add_argument('-H', '--highlight', metavar='PHRASE', nargs='+', help='List of words and phrases to search for, and highlight resulting sentences in output CSV')
+args = parser.parse_args()
+
+outputFile = open('filtered-' + args.inFile, 'w')
+outputWriter = csv.writer(outputFile)
+
+with open(args.inFile, 'r') as csv_file:
+     for row in csv.reader(csv_file):
+          newRow = []
+          for col in row:
+              if all(x not in col for x in args.filter):
+                   if args.highlight and any(y in col for y in args.highlight):
+                        newRow.append(col.upper())
+                   else:
+                        newRow.append(col)
+          outputWriter.writerow(newRow)
+
+    
